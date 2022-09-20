f518ba5b430187ee01317845638f9fa07893b19c
Vladyslav <vkadyrov@protonmail.com>
2022-09-20 18:45:43 -0400

    commit tree 5747e2ae63fdf1aa9a003af1fbc5ef2e2365221c

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    let message = 'This one is on me!'
+    return message
+  } else if (400 <= number && number < 2000) {
+    let message = 'That will be twenty bucks.'
+    return message
+  } else if (2000 < number && number < 2500) {
+    let message = 'I will gladly take your thirty bucks.'
+    return message
+  } else if (number >= 2500) {
+    let message = 'No can do.'
+    return message
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let message = city === 'NYC' ? "Ok, sounds good." : "No go."
+  return message
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  let answer;
+
+  switch (tip) {
+    case 'generous':
+      answer = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      answer = 'Thank you.';
+      break;
+    default:
+      answer = 'Bye.';
+    }
+    return answer;
 }
